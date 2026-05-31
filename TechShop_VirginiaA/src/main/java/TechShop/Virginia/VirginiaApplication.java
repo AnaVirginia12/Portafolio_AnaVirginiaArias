398978888629dca75612fcb8b80e877bea106412
AnaVirginia12 <aarias20495@ufide.ac.cr>
2026-05-30 22:56:00 -0600

    Commit2
    Commit semana2

--- TechShop_VirginiaA/src/main/java/TechShop/Virginia/VirginiaApplication.java
+++ TechShop_VirginiaA/src/main/java/TechShop/Virginia/VirginiaApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VirginiaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VirginiaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VirginiaApplication.class, args);
+    }
 }
+
