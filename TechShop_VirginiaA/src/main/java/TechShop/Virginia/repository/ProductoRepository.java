f3d9ee4718940ab4bbc6e06c33a1f3de340607e9
AnaVirginia12 <aarias20495@ufide.ac.cr>
2026-07-08 08:14:02 -0600

    Commit 7
    Semana6

---
+++ TechShop_VirginiaA/src/main/java/TechShop/Virginia/repository/ProductoRepository.java
@@ -0,0 +1,14 @@
+
+package TechShop.Virginia.repository;
+
+import TechShop.Virginia.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    
+    public List<Producto> findByActivoTrue();
+    
+}
